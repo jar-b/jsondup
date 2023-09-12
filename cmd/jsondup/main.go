208325e83da3950dfa2de2eaa79928b7666fe4c6
Jared Baker <jared.baker@hashicorp.com>
2023-09-12 14:11:54 -0400

    feat(cmd): accept arg, add help

--- cmd/jsondup/main.go
+++ cmd/jsondup/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jar-b/jsondup"
 )
 
+func init() {
+	// slightly better usage output
+	flag.Usage = func() {
+		fmt.Fprint(flag.CommandLine.Output(), "Detect duplicate keys in a JSON object.\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [filename]\n", os.Args[0])
+	}
+}
+
 func main() {
-	s := `{"a":"foo", "a":"bar"}`
-	if err := jsondup.ValidateNoDuplicateKeys(s); err != nil {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("unexpected number of arguments")
+	}
+
+	f := flag.Arg(0)
+	b, err := os.ReadFile(f)
+	if err != nil {
+		log.Fatalf("reading file: %s", err)
+	}
+
+	if err := jsondup.ValidateNoDuplicateKeys(string(b)); err != nil {
 		log.Fatal(err)
 	}
 }
