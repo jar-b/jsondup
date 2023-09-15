6aab2b8d65f7f5e0cd4e9fb7d902235128743f9a
Jared Baker <jared.baker@hashicorp.com>
2023-09-15 16:27:57 -0400

    chore: move usage out of init

--- cmd/jsondup/main.go
+++ cmd/jsondup/main.go
@@ -9,16 +9,14 @@ import (
 	"github.com/jar-b/jsondup"
 )
 
-func init() {
+func main() {
 	// slightly better usage output
 	flag.Usage = func() {
 		fmt.Fprint(flag.CommandLine.Output(), "Detect duplicate keys in a JSON object.\n\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [filename]\n", os.Args[0])
 	}
-}
-
-func main() {
 	flag.Parse()
+
 	if flag.NArg() != 1 {
 		log.Fatal("unexpected number of arguments")
 	}
